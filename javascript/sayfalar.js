3ddacebe82bb5c94658f0e204b3cc16d758893bb
Enes Hamza <89846189+eneshamza@users.noreply.github.com>
2024-01-30 17:04:30 +0300

    Update sayfalar.js

--- javascript/sayfalar.js
+++ javascript/sayfalar.js
@@ -1,21 +1,21 @@
 var anamenu=document.getElementById('anamenu')
 var hakkinda=document.getElementById('hakkinda')
 var iletisim=document.getElementById('iletisim')
-var duvarkagidi=document.getElementById('duvarkagidi')
+var video=document.getElementById('video')
 
 var anamenubuton = document.getElementById('anamenubuton')
 var hakkindabuton = document.getElementById('hakkindabuton')
 var iletisimbuton = document.getElementById('iletisimbuton')
-var duvarkagidibuton = document.getElementById('duvarkagidibuton')
+var videobuton = document.getElementById('video')
 
 anamenubuton.onclick = function () { 
   anamenu.style.display = 'block'
   hakkinda.style.display = 'none'
   iletisim.style.display = 'none'
-  duvarkagidi.style.display = 'none'
+  video.style.display = 'none'
   document.getElementById("hakkindabuton").classList.remove('active');
   document.getElementById("iletisimbuton").classList.remove('active');
-  document.getElementById("duvarkagidibuton").classList.remove('active');
+  document.getElementById("videobuton").classList.remove('active');
   document.getElementById("anamenubuton").classList.add('active');
 }
 
@@ -23,10 +23,10 @@ hakkindabuton.onclick = function () {
   anamenu.style.display = 'none'
   hakkinda.style.display = 'block'
   iletisim.style.display = 'none'
-  duvarkagidi.style.display = 'none'
+  video.style.display = 'none'
   document.getElementById("hakkindabuton").classList.add('active');
   document.getElementById("iletisimbuton").classList.remove('active');
-  document.getElementById("duvarkagidibuton").classList.remove('active');
+  document.getElementById("videobuton").classList.remove('active');
   document.getElementById("anamenubuton").classList.remove('active');
 }
 
@@ -34,20 +34,20 @@ iletisimbuton.onclick = function () {
   anamenu.style.display = 'none'
   hakkinda.style.display = 'none'
   iletisim.style.display = 'block'
-  duvarkagidi.style.display = 'none'
+  video.style.display = 'none'
   document.getElementById("hakkindabuton").classList.remove('active');
   document.getElementById("iletisimbuton").classList.add('active');
-  document.getElementById("duvarkagidibuton").classList.remove('active');
+  document.getElementById("videobuton").classList.remove('active');
   document.getElementById("anamenubuton").classList.remove('active');
 }
 
-duvarkagidibuton.onclick = function () { 
+videobuton.onclick = function () { 
   anamenu.style.display = 'none'
   hakkinda.style.display = 'none'
   iletisim.style.display = 'none'
-  duvarkagidi.style.display = 'block'
+  video.style.display = 'block'
   document.getElementById("hakkindabuton").classList.remove('active');
   document.getElementById("iletisimbuton").classList.remove('active');
-  document.getElementById("duvarkagidibuton").classList.add('active');
+  document.getElementById("videobuton").classList.add('active');
   document.getElementById("anamenubuton").classList.remove('active');
 }
