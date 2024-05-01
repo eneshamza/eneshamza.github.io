bd3db65762bb46b176f23bfd34ae679217bc40b5
Enes Hamza <89846189+eneshamza@users.noreply.github.com>
2024-05-01 19:45:11 +0300

    Update sayfalar.js

--- javascript/sayfalar.js
+++ javascript/sayfalar.js
@@ -2,21 +2,25 @@ var anamenu=document.getElementById('anamenu')
 var hakkinda=document.getElementById('hakkinda')
 var iletisim=document.getElementById('iletisim')
 var gelistirdiklerim=document.getElementById('gelistirdiklerim')
+var takaslik=document.getElementById('takaslik')
 
 var anamenubuton = document.getElementById('anamenubuton')
 var hakkindabuton = document.getElementById('hakkindabuton')
 var iletisimbuton = document.getElementById('iletisimbuton')
-var gelistirdiklerimbuton = document.getElementById('gelistirdiklerimbuton')
+var gelistirdiklerimbuton = document.getElementById('galistirdiklerimbuton')
+var takaslikbuton = document.getElementById('takaslikbuton')
 
 anamenubuton.onclick = function () { 
   anamenu.style.display = 'block'
   hakkinda.style.display = 'none'
   iletisim.style.display = 'none'
   gelistirdiklerim.style.display = 'none'
+  takaslik.style.display = 'none'
   document.getElementById("hakkindabuton").classList.remove('active');
   document.getElementById("iletisimbuton").classList.remove('active');
   document.getElementById("gelistirdiklerimbuton").classList.remove('active');
   document.getElementById("anamenubuton").classList.add('active');
+  document.getElementById("takaslikbuton").classList.remove('active');
 }
 
 hakkindabuton.onclick = function () { 
@@ -24,10 +28,12 @@ hakkindabuton.onclick = function () {
   hakkinda.style.display = 'block'
   iletisim.style.display = 'none'
   gelistirdiklerim.style.display = 'none'
+  takaslik.style.display = 'none'
   document.getElementById("hakkindabuton").classList.add('active');
   document.getElementById("iletisimbuton").classList.remove('active');
   document.getElementById("gelistirdiklerimbuton").classList.remove('active');
   document.getElementById("anamenubuton").classList.remove('active');
+  document.getElementById("takaslikbuton").classList.remove('active');
 }
 
 iletisimbuton.onclick = function () { 
@@ -35,10 +41,12 @@ iletisimbuton.onclick = function () {
   hakkinda.style.display = 'none'
   iletisim.style.display = 'block'
   gelistirdiklerim.style.display = 'none'
+  takaslik.style.display = 'none'
   document.getElementById("hakkindabuton").classList.remove('active');
   document.getElementById("iletisimbuton").classList.add('active');
   document.getElementById("gelistirdiklerimbuton").classList.remove('active');
   document.getElementById("anamenubuton").classList.remove('active');
+  document.getElementById("takaslikbuton").classList.remove('active');
 }
 
 gelistirdiklerimbuton.onclick = function () { 
@@ -46,8 +54,23 @@ gelistirdiklerimbuton.onclick = function () {
   hakkinda.style.display = 'none'
   iletisim.style.display = 'none'
   gelistirdiklerim.style.display = 'block'
+  takaslik.style.display = 'none'
   document.getElementById("hakkindabuton").classList.remove('active');
   document.getElementById("iletisimbuton").classList.remove('active');
   document.getElementById("gelistirdiklerimbuton").classList.add('active');
   document.getElementById("anamenubuton").classList.remove('active');
+  document.getElementById("takaslikbuton").classList.remove('active');
+}
+
+takaslikbuton.onclick = function () { 
+  anamenu.style.display = 'none'
+  hakkinda.style.display = 'none'
+  iletisim.style.display = 'none'
+  gelistirdiklerim.style.display = 'none'
+  takaslik.style.display = 'block'
+  document.getElementById("hakkindabuton").classList.remove('active');
+  document.getElementById("iletisimbuton").classList.remove('active');
+  document.getElementById("gelistirdiklerimbuton").classList.remove('active');
+  document.getElementById("anamenubuton").classList.remove('active');
+  document.getElementById("takaslikbuton").classList.add('active');
 }
